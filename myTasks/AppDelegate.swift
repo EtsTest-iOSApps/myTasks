307805f69a3767ab0097a79ba06015cbb1a7d4df
Maksim Pavlovich <max.pav.love@icloud.com>
2021-11-05 18:34:47 +0300

    Completed ToDoList app

--- myTasks/AppDelegate.swift
+++ myTasks/AppDelegate.swift
@@ -10,13 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         RealmSetup.performRealmMigrations()
         return true
-        
     }
     
 }
@@ -34,4 +31,3 @@ func application(_ application: UIApplication, didDiscardSceneSessions sceneSess
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
-
